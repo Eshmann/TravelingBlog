5678e8a605eb28deabc3226cae95f2798ca88270
Oleksandr <sashadanilenko@mail.ua>
2018-11-08 13:49:56 +0200

    Create dependencies / Test variant

--- TravelingBlog/Startup.cs
+++ TravelingBlog/Startup.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AdventureDb.Core.Repositories;
+using AdventureDb.Persistence.Repositories;
+using Autofac;
+using Autofac.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +31,12 @@ namespace TravelingBlog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<IPostBlogRepository, PostBlogRepository>();
+            services.AddTransient<ICountryRepository, CountryRepository>();
+            services.AddTransient<ITagRepository, TagRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ITripRepository, TripRepository>();
+            services.AddTransient<IRoleRepository, RoleRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
