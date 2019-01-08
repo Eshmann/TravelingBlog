d0297556b2ab38053f5d528bd93a4ca9c2b7c811
Oleh Radiuk <radyukolegg@gmail.com>
2019-01-08 18:01:21 +0200

    fixed

--- TravelingBlog.Angular/src/app/dashboard/home/home.component.ts
+++ TravelingBlog.Angular/src/app/dashboard/home/home.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { HomeDetails } from '../models/home.details.interface';
 import { DashboardService } from '../services/dashboard.service';
 import { SubscriberDetails } from '../models/subsciber.details.interface';
+import { TripserviceService } from '../services/tripservice.service';
+import { TripDetails } from '../models/trip.details.interface';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +14,10 @@ import { SubscriberDetails } from '../models/subsciber.details.interface';
 export class HomeComponent implements OnInit {
 
   homeDetails: HomeDetails;
+  topTrips:TripDetails[];
 
-  constructor(private dashboardService: DashboardService) { }
+  constructor(private dashboardService: DashboardService, private tripservice:
+    TripserviceService) { }
 
   ngOnInit() {
 
@@ -23,12 +27,13 @@ export class HomeComponent implements OnInit {
       },
         error => {
           // this.notificationService.printErrorMessage(error);
-        });
-    
-    
-    
-
-
+        }); 
+        this.getTopTrips();
   }
-
+  getTopTrips(){
+    this.tripservice.getTopTrips().subscribe(res=>{
+      this.topTrips = [...res]; 
+      console.log(res)
+    })
+  }
 }
